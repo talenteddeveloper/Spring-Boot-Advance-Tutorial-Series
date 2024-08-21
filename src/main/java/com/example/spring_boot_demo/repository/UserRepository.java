4e06f01e8d11e42f62c90f69d6388a31d04b6318
Talented Developer <alwaysrohitsingh@outlook.com>
2024-08-21 11:43:10 +0530

    Authenticating Spring Boot Users with Database Credentials

--- src/main/java/com/example/spring_boot_demo/repository/UserRepository.java
+++ src/main/java/com/example/spring_boot_demo/repository/UserRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.spring_boot_demo.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long>  {
+	User findByUsername(String username);
 }
