edf51738b9a71490f4f5daf5c22d20959be88939
Talented Developer <alwaysrohitsingh@outlook.com>
2024-08-21 11:19:49 +0530

    Authenticating Spring Boot Users with Database Credentials

--- src/main/java/com/example/spring_boot_demo/config/SecurityConfig.java
+++ src/main/java/com/example/spring_boot_demo/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.example.spring_boot_demo.config;
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -12,30 +14,48 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.example.spring_boot_demo.service.CustomUserDetailsService;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+	
+	@Autowired
+	private CustomUserDetailsService customUserDetailsService;
+	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests(
-				requests -> requests.requestMatchers("/api/users/**").authenticated().anyRequest().permitAll()
-
-		).formLogin(withDefaults()).httpBasic(withDefaults());
+		http
+		.csrf(csrf -> csrf.ignoringRequestMatchers("/api/users/createUser"))
+		.authorizeHttpRequests(requests -> requests
+					.requestMatchers("/api/users/createUser").permitAll()
+				.requestMatchers("/api/users/**").authenticated()
+				.anyRequest().permitAll()
+				)
+			.formLogin(withDefaults())
+			.httpBasic(withDefaults());
 		return http.build();
 	}
 	
-  @Bean
-  public UserDetailsService userDetailsService()
-  {
-  	var user = User.builder()
-  			.username("user")
-  			.password(passwordEncoder().encode("password"))
-  			.roles("USER")
-  			.build();
-  	
-  	return new InMemoryUserDetailsManager(user);
-  }
-  
+//  @Bean
+//  public UserDetailsService userDetailsService()
+//  {
+//  	var user = User.builder()
+//  			.username("user")
+//  			.password(passwordEncoder().encode("password"))
+//  			.roles("USER")
+//  			.build();
+//  	
+//  	return new InMemoryUserDetailsManager(user);
+//  }
+	
+	@Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+    	DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+    	daoAuthenticationProvider.setUserDetailsService(customUserDetailsService);
+    	daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+    	return daoAuthenticationProvider;
+    }
   @Bean
   public PasswordEncoder passwordEncoder(){
   	return new BCryptPasswordEncoder();
